fabb9b3c5aa9eae60bdaeca100be6713a67ef07b
John Agbaje <agbajeakin84@gmail.com>
2022-12-08 14:19:00 -0800

    dddge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+
 int main(void)
-{ I
-printf("\"Programming is like building a multilingual puzzle");
-return 0;
-}
+
+{
+
+ printf("\"Programming is like building a multilingual puzzle\n");
+
+  return 0;
+
