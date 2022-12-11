fa1a62a06b2e7241713c9ff3c1a845d35f857707
John Agbaje <agbajeakin84@gmail.com>
2022-12-11 14:33:31 -0800

    e

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		putchar(k);
+	}
+	printf("\n");
+
+	return (0);
+
+}
+
