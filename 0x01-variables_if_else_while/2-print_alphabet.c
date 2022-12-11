973cff45691ff6955e91a3d0cf8eef50d89951d6
John Agbaje <agbajeakin84@gmail.com>
2022-12-10 16:12:29 -0800

    sksks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	for (char m = 'a'; m <= 'z'; m++)
+	{
+		putchar("%c", m);
+	}
+
+	putchar("\n";
+			)
+	return (0);
+
+}
+
