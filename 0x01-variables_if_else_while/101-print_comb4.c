b3ddef918932f69ed6e3c817a132c84112536385
John Agbaje <agbajeakin84@gmail.com>
2022-12-12 06:53:36 -0800

    s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+
+	{
+
+	for (b = '0'; b <= '9'; b++)
+
+	{
+
+	for (c = '0'; c <= '9'; c++)
+
+	{
+
+	if (a != b && b != c && c != a)
+
+	{
+
+	putchar(a);
+
+	putchar(b);
+
+	putchar(c);
+
+	putchar(',');
+
+	putchar(' ');
+
+	}
+
+	}
+
+	}
+
+	}
+	return (0);
+}
