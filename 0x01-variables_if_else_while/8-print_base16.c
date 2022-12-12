c065fea0449371d6a0a18eef38e045bf21d4f1e1
John Agbaje <agbajeakin84@gmail.com>
2022-12-12 05:24:57 -0800

    s

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -16,16 +16,15 @@ int main(void)
 	d = 0;
 
 	while  (d < 10)
-	
-		{
-			putchar(d + '0');
-			d++;
-		}
+	{
+		putchar(d + '0');
+		d++;
+	}
 	while (i <= 'f')
-		{
-			 putchar(i);
-		 	i++;
-		}
+	{
+		putchar(i);
+		i++;
+	}
 
 	putchar('\n');
 	return (0);
