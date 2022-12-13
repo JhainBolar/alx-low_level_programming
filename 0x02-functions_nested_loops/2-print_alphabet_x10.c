c752d6b691eefd676496f8570dcc81e7f1bae667
John Agbaje <agbajeakin84@gmail.com>
2022-12-13 15:22:42 -0800

    aa

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,38 @@
+nclude "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - print letters a-z times 10
+ *
+ * * Return: Returns void.
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 1;
+
+	int b = 10;
+
+	while (a <= b)
+
+	{
+
+	char c = 'a';
+
+	while (c < 'z')
+
+	{
+
+	_putchar(c);
+
+	c++;
+
+	}
+
+	a++;
+
+	}
+
+	_putchar('\n');
+
+	return (0);
