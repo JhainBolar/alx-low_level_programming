2376c1789dbbbdc5c17302d4e6d22ebf1a71c27e
John Agbaje <agbajeakin84@gmail.com>
2022-12-14 02:31:05 -0800

    derf

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ *  @x: An integre input
+ *
+ * Return: Absolute value of number r
+ */
+int print_last_digit(int x)
+{
+	int x;
+
+	int d = x % 10;
+
+	printf("%d", d);
+
+}	
