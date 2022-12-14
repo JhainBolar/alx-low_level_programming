04aba35d44a4524f3d83055bec412f86d1a033a1
John Agbaje <agbajeakin84@gmail.com>
2022-12-14 14:26:42 -0800

    giik

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * add - return the sum of parameters
+ * add - return the sum of its parameters
  *
  * @x: int type number
  *
  * @y: int type number
  */
 
+
 int add(int x, int y)
 {
-	 return (x + y);
+	return (x + y);
 }
 
