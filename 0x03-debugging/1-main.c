8c01f3114dabd71dfa9558955671b672f9b7bd4c
John Agbaje <agbajeakin84@gmail.com>
2022-12-15 02:08:40 -0800

    sqw

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - causes and inifinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+
+
