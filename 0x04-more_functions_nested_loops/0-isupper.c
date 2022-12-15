d42beac556f02653a0bf747464ae3feb918e27be
John Agbaje <agbajeakin84@gmail.com>
2022-12-15 04:25:10 -0800

    nest

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - Checks if c is an upper case
+ *  @c: input character
+ *  Return: Returns 1 if c is upper otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		return 1;
+	}
+
+       	else 
+
+	{
+		return 0;
+	}
+}
