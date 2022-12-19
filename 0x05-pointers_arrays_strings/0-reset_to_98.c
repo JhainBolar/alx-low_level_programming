513107aa7bbedb56b573cf6ae905f86c27dca5d5
John Agbaje <agbajeakin84@gmail.com>
2022-12-19 03:54:53 -0800

    nu

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - update the calue of the argument
+*
+* @n: integer
+*
+* Return - Always 0
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
