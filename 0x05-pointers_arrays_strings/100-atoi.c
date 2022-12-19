7a9581e660e2f75097b5852b3328284c418e05c7
John Agbaje <agbajeakin84@gmail.com>
2022-12-19 14:42:29 -0800

    git

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _atoi - int
+* @s: pointer
+* Return: int.
+*/
+
+
+int _atoi(char *s)
+{
+int result;
+s = "123456";
+result = _atoi(s);
+printf("%d\n", result);
+return (0);
+}
+
