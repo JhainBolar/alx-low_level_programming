1c42e7393a69079f4b8cef854ae4ee64c76f8364
John Agbaje <agbajeakin84@gmail.com>
2022-12-22 00:25:48 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,23 +10,35 @@
 */
 
 
-
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int dest_len;
-dest_len = 0;
 
+int srclen = 0, i = 0;
+char *temp = dest, *start = src;
+while (*src)
 
-while (dest[dest_len] != '\0')
 {
-dest_len++;
+
+srclen++;
+
+src++;
+
 }
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-dest[dest_len + i] = src[i];
-dest[dest_len + i] = '\0';
-}
-return (dest);
+while (*dest)
+
+dest++;
+
+if (n > srclen)
+
+n = srclen;
+
+src = start;
+
+for (; i < n; i++)
+
+*dest++ = *src++;
+*dest = '\0';
+return (temp);
+
 }
